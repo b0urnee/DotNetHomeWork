109604a7a43c28ef4d3c85d4e72603beff932ce3
luoxiao <2769086541@qq.com>
2021-04-07 00:48:23 +0800

    homework5

---
+++ homework5/OMS/Order.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OMS
+{
+    class Order
+    {
+        //订单号，顾客，数量，下单时间，配送地址
+        public int OrderNum { get; set; }
+        public string Customer { get; set; }
+        public string OrderTime { get; set; }
+        public string OrderAddress { get; set; }
+
+        public Order(int orderNum, string customer, string orderTime, string orderAddress)
+        {
+            this.OrderNum = orderNum;
+            this.Customer = customer;
+            this.OrderTime = orderTime;
+            this.OrderAddress = orderAddress;
+        }
+
+        public List<OrderDetails> orderDetails = new List<OrderDetails>();
+
+        public override bool Equals(object obj)
+        {
+            return obj is Order order && OrderNum == order.OrderNum;
+        }
+        public int CompareTo(object obj)
+        {
+            Order tmp = obj as Order;
+            return this.OrderNum.CompareTo(tmp.OrderNum);
+        }
+        public override int GetHashCode()
+        {
+            return OrderNum.GetHashCode() + 1;
+        }
+        public Boolean SelectGoods(string a)
+        {
+            Boolean b = false;
+            foreach (var tem in orderDetails)
+            {
+                if (tem.Goods == a) { b = true; }
+            }
+
+            return b;
+        }
+        public override string ToString()
+        {
+            return $"订单号为{OrderNum},顾客是{Customer},下单时间{OrderTime},配送地址{OrderAddress}";
+        }
+
+        //增添
+        public void AddItems(OrderDetails a)
+        {
+            Boolean IsAdd = false;
+            foreach (OrderDetails temp in orderDetails)
+            {
+                if (a.Equals(temp))
+                {
+                    Console.WriteLine("商品已经存在，不需要添加");
+                    IsAdd = true;
+                }
+            }
+            if (IsAdd == false) orderDetails.Add(a);
+        }
+        //移除一个订单明细
+        public void Remove(string goods)
+        {
+            Boolean SuccessRemove = false;
+            for (int i = 0; i < orderDetails.Count; i++)
+            {
+                if (orderDetails[i].Goods == goods)
+                {
+                    orderDetails.Remove(orderDetails[i]);
+                    SuccessRemove = true;
+                }
+            }
+            if (SuccessRemove == true)
+            {
+                Console.WriteLine($"订单中名为{goods}的商品移除成功");
+            }
+            else Console.WriteLine($"找不到名为{goods}的商品，移除失败");
+        }
+        //计算总金额
+        public double Sum()
+        {
+            double sum = 0;
+            foreach (var temp in orderDetails)
+            {
+                sum = sum + temp.Num * temp.Price;
+            }
+            return sum;
+        }
+    }
+}
---
+++ homework5/OMS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService service = new OrderService();
+            Order p1 = new Order(14860231, "小李", "2021.1.1", "湖南路22号");
+            Order p2 = new Order(31232144, "小马", "2021.2.2", "中山路89号");
+            Order p3 = new Order(11234254, "小王", "2021.3.5", "上海路23号");
+            OrderDetails g1 = new OrderDetails("水杯", 5.0, 1);
+            OrderDetails g2 = new OrderDetails("椅子", 10.0, 1);
+            OrderDetails g3 = new OrderDetails("台灯", 20.0, 1);
+            OrderDetails g4 = new OrderDetails("书包", 100.0, 1);
+            p1.AddItems(g1);
+            p1.AddItems(g2);
+            p2.AddItems(g3);
+            p2.AddItems(g4);
+            p3.AddItems(g1);
+            p3.AddItems(g3);
+            service.AddItems(p1);
+            service.AddItems(p2);
+            Console.WriteLine("订单情况：\n" + service.ToString());
+            service.AddItems(p3);
+            Console.WriteLine("增加一个订单后：\n" + service.ToString());
+            service.Remove(31232144);
+            Console.WriteLine("删除一个订单后：\n" + service.ToString());
+            Console.WriteLine();
+        }
+    }
+}
